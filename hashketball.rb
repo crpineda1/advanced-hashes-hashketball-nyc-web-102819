0774068dddf95b3c9404d6981fb8439d1437c09e
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-26 17:18:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,13 +162,13 @@ def shoe_size (player_name)
 end
 
 #team colors
-def team_colors(team_name)
-  game_hash.each do |side,team|
-    if team[:team_name] == team_name
-      return team[:colors]
-    end
-  end
-end
+# def team_colors(team_name)
+#   game_hash.each do |side,team|
+#     if team[:team_name] == team_name
+#       return team[:colors]
+#     end
+#   end
+# end
 
 #binding.pry
 
